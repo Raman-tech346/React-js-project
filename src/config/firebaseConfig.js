6d0eb04799b29b4fd8acbd84655b4d5ae1fb73c8
Ramandeep singh <rs3842212@gmail.com>
2022-05-23 15:27:38 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyA-yrIju6JGdO3lzFnsFSNEgnKWJnrSshQ",
+    authDomain: "mygitapp-d8a92.firebaseapp.com",
+    projectId: "mygitapp-d8a92",
+    storageBucket: "mygitapp-d8a92.appspot.com",
+    messagingSenderId: "614498949683",
+    appId: "1:614498949683:web:d8212f8c0d0822e494a146",
+    measurementId: "G-ZGLYX33FGM"
+}
---
+++ src/layout/Footer.js
@@ -0,0 +1,12 @@
+import REACT from "react";
+import { Container } from "reactstrap";
+
+const Footer = () => {
+    return(
+        <Container fluid tag="footer" className="text-center bg-info text-white text-uppercase fixed-bottom p-3">
+         github search app with firebase
+        </Container>
+    )
+}
+
+export default Footer;
